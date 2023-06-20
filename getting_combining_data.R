26ab19b036847fd32fe7b32d82548d2c52f3c9da
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2023-06-20 16:49:55 +0200

    Initial upload of data and code

---
+++ getting_combining_data.R
@@ -0,0 +1,98 @@
+### Downloading and combining data from NCBI Genbank and BOLD
+### https://fkeck.github.io/refdb/articles/ncbi_bold.html
+# data management libraries
+library(readxl)
+library(stringr)
+library(dplyr)
+# gene data
+library(refdb)
+library(ape)
+library(bold)
+library(seqinr)
+library(rentrez)
+
+# reading data from soortenlijst (https://doi.org/10.5281/zenodo.5596205)
+# Duistermaat, H., Sparrius, L.B., & Denters, T. (2021). Standaardlijst van de Nederlandse Flora 2020 [Data set]. In Gorteria. Zenodo. https://doi.org/10.5281/zenodo.5596206
+soortenlijst_nl <- read_xlsx("data/SL2020 Checklist Flora NL.xlsx", sheet = 2)
+# splitting genus and species
+soortenlijst_nl$genus <- word(soortenlijst_nl$`Wetenschappelijke naam`,1)
+soortenlijst_nl$species <- str_replace(soortenlijst_nl$`Wetenschappelijke naam`, paste0(soortenlijst_nl$genus, " "), "")
+
+#trnL_ncbi <- refdb_import_NCBI("trnL voucher") # results in much data -> slow download
+sink("log/ncbi_results.log", append=TRUE, split=TRUE)
+for (i in 1:nrow(soortenlijst_nl)) {
+     message(paste0("Finding data for ", soortenlijst_nl$`Wetenschappelijke naam`[i]))
+     trnL_ncbi_temp <- refdb_import_NCBI(paste0("trnL voucher ", soortenlijst_nl$`Wetenschappelijke naam`[i]))
+     if (!is.null(nrow(trnL_ncbi_temp))) {
+          if (exists("trnL_ncbi")) {
+               trnL_ncbi <- rbind(trnL_ncbi, trnL_ncbi_temp)
+               } else {
+                    trnL_ncbi <- trnL_ncbi_temp
+               }
+     }
+     time.sleep(2) # to prevent errors (?)
+}
+sink()
+
+trnL_ncbi1 <- trnL_ncbi # first round: 10499 observations (no log)
+rm(trnL_ncbi)
+trnL_ncbi2 <- trnL_ncbi # second round 9045 observations
+file.rename("log/ncbi_results.log", "log/ncbi_results_2.log")
+rm(trnL_ncbi)
+trnL_ncbi3 <- trnL_ncbi # third round 6766 observations, with HTTP error
+file.rename("log/ncbi_results.log", "log/ncbi_results_3.log")
+rm(trnL_ncbi)
+trnL_ncbi4 <- trnL_ncbi # fourth round 10499 observations (same as first round)
+file.rename("log/ncbi_results.log", "log/ncbi_results_4.log")
+rm(trnL_ncbi)
+# to make sure all data is there, the data is combined and then filtered for unique records
+trnL_ncbi <- rbind(trnL_ncbi1, trnL_ncbi2, trnL_ncbi3, trnL_ncbi4)
+trnL_ncbi <- unique(trnL_ncbi)
+# filter data on trnL gene and on species in soortenlijst
+trnL_ncbi <- trnL_ncbi %>%
+     filter(gene=="trnL") %>%
+     filter(species %in% soortenlijst_nl$`Wetenschappelijke naam`)
+
+
+# read data from bold using refdb-package (slow)
+for (i in 1:nrow(soortenlijst_nl)) {
+     message(paste0("Finding data for ", soortenlijst_nl$`Wetenschappelijke naam`[i]))
+     soorten_bold_temp <- refdb_import_BOLD(taxon=soortenlijst_nl$`Wetenschappelijke naam`[i])
+     if (!is.null(nrow(trnL_bold_temp))) {
+          if (exists("soorten_bold")) {
+               soorten_bold <- rbind(soorten_bold, soorten_bold_temp)
+          } else {
+               soorten_bold <- soorten_bold_temp
+          }
+     }
+}
+# filter marker and check with soortenlijst
+trnL_bold <- soorten_bold %>%
+     filter(markercode == "trnL") %>%
+     filter(species %in% soortenlijst_nl$`Wetenschappelijke naam`) %>%
+     distinct()
+
+# get data from bold on marker using bold-package, faster but less info
+bold_records <- bold_seq(marker = "trnL")
+# join soortenlijst with bold-data
+nl_soorten_bold <- soortenlijst_nl %>%
+     select(soortnummer,`Wetenschappelijke naam`,`Nederlandse naam`, Familie, genus, species) %>%
+     inner_join(bold_records, by = c("Wetenschappelijke naam" = "identification"))
+
+
+trnL_bold_ncbi <- refdb_merge(trnL_bold, trnL_ncbi)
+
+
+
+
+# check from here [!!!]
+test <- read.GenBank("14716574")
+
+# export to nexus
+
+write.nexus.data(trnL_bold_ncbi, file="export/trnl_data.nexus",
+                 format="protein")
+write.dna(trnL_bold_ncbi, "export/trnl_data.dna")
+write.csv(trnL_bold_ncbi, "export/trnl_data.csv", row.names = FALSE)
+# check from here
+refdb_export_dada2
